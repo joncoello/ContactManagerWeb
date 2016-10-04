8d77da4018f085ee8adc1ee5057dad12ef0873b9
Jon Coello <joncoello@gmail.com>
2016-10-04 22:43:10 +0100

    introduce chutzpah for integrated vs testing

--- ContactManagerWeb/app/app.component.spec.ts
+++ ContactManagerWeb/app/app.component.spec.ts
@@ -1,37 +1,21 @@
 /* tslint:disable:no-unused-variable */
 import { AppComponent } from './app.component';
 
-import { TestBed } from '@angular/core/testing';
-
-import { By }             from '@angular/platform-browser';
-
 ////////  SPECS  /////////////
 
-/// Delete this
-describe('Smoke test', () => {
-  it('should run a passing test', () => {
-    expect(true).toEqual(true, 'should pass');
-  });
-});
+describe('AppComponent', function () {
 
-describe('AppComponent with TCB', function () {
-  beforeEach(() => {
-    TestBed.configureTestingModule({declarations: [AppComponent]});
-  });
+    var sut: AppComponent;
 
-  it('should instantiate component', () => {
-    let fixture = TestBed.createComponent(AppComponent);
-    expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
-  });
+    beforeEach(() => {
+        sut = new AppComponent();
+    });
 
-  it('should have expected <h1> text', () => {
-    let fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
+    it('should create', () => {
+        expect(sut instanceof AppComponent).toBe(true, 'should create AppComponent');
+    });
 
-    let h1 = fixture.debugElement.query(el => el.name === 'h1').nativeElement;  // it works
-
-        h1 = fixture.debugElement.query(By.css('h1')).nativeElement;            // preferred
-
-    expect(h1.innerText).toMatch(/angular 2 app/i, '<h1> should say something about "Angular 2 App"');
-  });
+    it('should start with the correct title', () => {
+        expect(sut.title).toEqual('My First Angular 2 App');
+    });
 });
--- ContactManagerWeb/app/app.component.ts
+++ ContactManagerWeb/app/app.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>{{title}}</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+    title: string;
+
+    constructor() {
+        this.title = "My First Angular 2 App";
+    }
+}
