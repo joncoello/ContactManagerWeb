ecf38e656596f777f9d7d747b35c8c69c96c1139
Jon Coello <joncoello@gmail.com>
2016-10-04 23:04:09 +0100

    add contact list component

---
+++ ContactManagerWeb/app/app.contactListComponent.spec.ts
@@ -0,0 +1,22 @@
+﻿/* tslint:disable:no-unused-variable */
+import { ContactListComponent } from './app.contactListComponent';
+
+////////  SPECS  /////////////
+describe('ContactListComponent', function () {
+
+    var sut: ContactListComponent;
+
+    beforeEach(() => {
+        sut = new ContactListComponent();
+    });
+
+    it('should create', () => {
+        expect(sut instanceof ContactListComponent).toBe(true, 'should create');
+    });
+
+    it('should start with the correct title', () => {
+        expect(sut.title).toEqual('Contact List');
+    });
+
+
+});
---
+++ ContactManagerWeb/app/app.contactListComponent.ts
@@ -0,0 +1,15 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'contact-list',
+    template: 'Contact List'
+})
+export class ContactListComponent {
+
+    title: string;
+
+    constructor() {
+        this.title = 'title';
+    }
+
+}
